1bd89d12f258cdba96410bc798867eabbe2b662b
davidas56 <85710968+davidas56@users.noreply.github.com>
2022-01-25 22:44:30 -0500

    Update aplicacion.js

--- POO/app/aplicacion.js
+++ POO/app/aplicacion.js
@@ -30,6 +30,25 @@ class App{
             alert('app desinstalada corectamente')
         }
     }
+    appinfo(){
+        return `
+        Descargar : <b>${this.descargar}</b><br>
+        Puntuacion : <b>${this.puntuacion}</b><br>
+        Peso : <b>${this.peso}</b><br>
+        `
+    }
 }
 
-app1 = new App ("18,000"," 5 estrellas","900mb")
+app = new App ("18,000"," 5 estrellas","900mb");
+app2 = new App ("15,000"," 4 estrellas","400mb");
+app3 = new App ("10,000"," 3 estrellas","500mb");
+app4 = new App ("9,000"," 2 estrellas","600mb");
+app5 = new App ("19,000"," 4 estrellas","800mb");
+
+document.write(`
+  ${app.appinfo()} <br>
+  ${app2.appinfo()} <br>
+  ${app3.appinfo()} <br>
+  ${app4.appinfo()} <br>
+  ${app5.appinfo()} <br>
+`)
